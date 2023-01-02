5389e74a0ff48aafeafc572b3b87149ca15d5b39
ana cecilia martinez <ana.cecilia.martinez12@gmail.com>
2023-01-02 13:42:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
