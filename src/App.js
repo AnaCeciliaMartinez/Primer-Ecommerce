a1185e770583d931eaebb7378d97fd7e7eba4d14
ariel sosa <ariel.sosa.carlos89@gmail.com>
2023-02-01 16:36:04 -0300

    Products ensamblado con Json

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
 import './index.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Cart from './components/Cart';
 import HamMenu from './components/HamMenu';
-import { useState } from 'react';
 import Home from './components/Home'
 import {Routes, Route} from  'react-router-dom'
 import Products from './components/Products';
 
-const lista = [
-    {
-      id: 1,
-      image: <img src="https://m.media-amazon.com/images/I/61rODsR2DKL._AC_.jpg" 
-      alt ="pathfinder"
-      className="w-40 h-40 object-contain rounded-xl mb-2"></img>,
-      name: "Pathfinder",
-      price: "$10000"
-    },
-    {
-      id: 2,
-      image: <img src="https://m.media-amazon.com/images/I/61rODsR2DKL._AC_.jpg" 
-      alt ="pathfinder"
-      className="w-40 h-40 object-contain rounded-xl mb-2"></img>,
-      name: "Pathfinder",
-      price: "$10000"
-    },
-    {
-      id: 3,
-      image: <img src="https://m.media-amazon.com/images/I/61rODsR2DKL._AC_.jpg" 
-      alt ="pathfinder"
-      className="w-40 h-40 object-contain rounded-xl mb-2"></img>,
-      name: "Pathfinder",
-      price: "$10000"
-    },
-    {
-      id: 4,
-      image: <img src="https://m.media-amazon.com/images/I/61rODsR2DKL._AC_.jpg" 
-      alt ="pathfinder"
-      className="w-40 h-40 object-contain rounded-xl mb-2"></img>,
-      name: "Pathfinder",
-      price: "$10000"
-    },
-    {
-      id: 5,
-      image: <img src="https://m.media-amazon.com/images/I/61rODsR2DKL._AC_.jpg" 
-      alt ="pathfinder"
-      className="w-40 h-40 object-contain rounded-xl mb-2"></img>,
-      name: "Pathfinder",
-      price: "$10000"
-    },
-]
+const catalogue = []
 
 function App() {
 
@@ -58,13 +18,28 @@ function App() {
   const toggleOpen = () =>{
     setIsOpen(!isOpen);
   };
+  
+  const [lista, setlista] = useState(catalogue)
+
+  const getData = async () => {
+    const ENDPOINT = " http://localhost:5000/products";
+  
+    const res = await axios.get(ENDPOINT),
+      productsdb = await res.data;
+
+      setlista(productsdb)
+  }
+
+  useEffect (() => {
+      getData() 
+  }, []) 
 
   return (
-    <div className="bg-[#141414] min-h-screen">
+    <div className="bg-[#141414] min-h-screen space-y-5">
       <NavBar  openClose={toggleOpen} />
       {isOpen && <HamMenu />}
       <Routes>
-        <Route path='/' element={<Home />}/> 
+        <Route path='/' element={<Home lista={lista}/>}/> 
         <Route path='/products' element={<Products lista={lista}/>}/>
       </Routes>
       <Footer/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import Carousel from './Carousel'
+import CardList from './CardList'
 
-const Home = () => {
+const listaHome = [];
+
+const Home = (props) => {
     return (
         <main className="w-full space-y-10">
         <div className='flex content-center justify-center'>
@@ -13,94 +16,9 @@ const Home = () => {
         <div>
             <h1 className='text-[#4f46e5] text-center font-serif underline text-2xl md:text-4xl'>Conoce nuestros Juegos</h1>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-5">
-        <div className="p-4">
-          <img 
-            src="https://cdn.pixabay.com/photo/2017/09/14/02/11/domino-2747758_960_720.jpg"
-            alt=""
-            className="w-full h-60 object-cover rounded-xl mb-2"
-          />
-          <h3 className="text-gray-300 text-lg">Domino</h3>
-          <p className="text-gray-500 mb-3">juegos de mesa</p>
-          <div>
-          <span className="bg-gray-50 p-1 text-green-800 rounded-lg"> -60% </span>
-          <span className="text-gray-500 p-2">$1000</span>
-          <span className="text-white">$400 </span>
-          <button className="bg-[#4f46e5] text-white-2 rounded-lg w-full mt-2 bg-gray-50">
-            Agregar al carrito
-          </button>
-        </div>
-        </div>
-        <div className="p-4">
-          <img
-            src="https://cdn.pixabay.com/photo/2015/03/04/10/58/cube-658643_960_720.jpg"
-            alt=""
-            className="w-full h-60 object-cover rounded-xl mb-2"
-          />
-          <h3 className="text-gray-300 text-lg">Dados</h3>
-          <p className="text-gray-500 mb-3 ">juegos de mesa</p>
-          <div className="flex items-center gap-1 "></div>
-          <div>
-          <span className="bg-gray-50 p-1 text-green-800 rounded-lg"> -50% </span>
-          <span className="text-gray-500 p-2">$1000</span>
-          <span className="text-white">$500 </span>
-          <button className="bg-[#4f46e5] text-white-2 rounded-lg w-full mt-2 bg-gray-50">
-            Agregar al carrito
-          </button>
-        </div>
-        </div>
-        <div className="p-4">
-          <img
-            src="https://cdn.pixabay.com/photo/2016/04/27/10/16/monopoly-1356307_960_720.jpg"
-            alt=""
-            className="w-full h-60 object-cover rounded-xl mb-2"
-          />
-          <h3 className="text-gray-300 text-lg">Monopoli</h3>
-          <p className="text-gray-500 mb-3">juegos de mesa</p>
-          <div>
-          <span className="bg-gray-50 p-1 text-green-800 rounded-lg"> -80% </span>
-          <span className="text-gray-500 p-2">$1000</span>
-          <span className="text-white">$200 </span>
-          <button className="bg-[#4f46e5] text-white-2 rounded-lg w-full mt-2 bg-gray-50">
-            Agregar al carrito
-          </button>
-        </div>
-        </div>
-        <div id="hola" className="p-4">
-          <img
-            src="https://cdn.pixabay.com/photo/2021/01/29/11/33/chess-5960730_960_720.jpg"
-            alt=""
-            className="w-full h-60 object-cover rounded-xl mb-2 text-center"
-          />
-          <h3 className="text-gray-300 text-lg">Ajedrez</h3>
-          <p className="text-gray-500 mb-3">juegos de mesa</p>
-          <div className="flex items-center gap-2"></div>
         <div>
-          <span className="bg-gray-50 p-1 text-green-800 rounded-lg"> -80% </span>
-          <span className="text-gray-500 p-2">$1000</span>
-          <span className="text-white">$200 </span>
-          <button className="bg-[#4f46e5] text-white-2 rounded-lg w-full mt-2 bg-gray-50">
-            Agregar al carrito
-          </button>
-        </div>
-        </div>
-        <div className="p-4">
-            <img
-            src="https://cdn.pixabay.com/photo/2015/05/15/18/21/cards-769040_960_720.jpg"
-            alt=""
-            className="w-full h-60 object-cover rounded-xl mb-2 text-center"
-            />
-            <h3 className="text-gray-300 text-lg ">Cartas</h3>
-            <p className="text-gray-500 mb-3">juegos de mesa </p>
-            <div>
-            <span className="bg-gray-50 p-1 text-green-800 rounded-lg"> -40% </span>
-            <span className="text-gray-500 p-2">$1000</span>
-            <span className="text-white">$400 </span>
-            <button className="bg-[#4f46e5] text-white-2 rounded-lg w-full mt-2 bg-gray-50">
-            Agregar al carrito
-            </button>
-        </div>
-        </div>
+
+          <CardList lista={props.lista.filter(lista => lista.category === "Clasico")}/>
         </div>
         <div className='flex contet-center justify-center'>
             <div className='w-4/5 h-48 bg-[url(https://media.istockphoto.com/id/1300208115/es/vector/tri%C3%A1ngulo-cruz-cuadrado-y-c%C3%ADrculo-ilustraci%C3%B3n-rectangular-sobre-fondo-azul-oscuro.jpg?s=612x612&w=0&k=20&c=hdsUG53ADWQ7jtAmyQte6bGf_wVPvKN6IhGi-g9GIEA=)] content-center'>
